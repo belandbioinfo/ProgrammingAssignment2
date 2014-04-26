e51b2d1525f67cb5ecbda5919825342254b0feae
kbeland <kbeland@unomaha.edu>
2014-04-26 09:09:23 -0500

    Added comments explaining the functions contained in the file.

--- cachematrix.R
+++ cachematrix.R
@@ -1,5 +1,13 @@
 ## Put comments here that give an overall description of what your
 ## functions do
+##
+## The following R functions create a special object "Vector" that 
+## contains functions for curation of a cache. This cache contains
+## the result of a matrix inversion if one has already been calculated 
+## for that particular object, and if not it contains a function
+## for inverting the matrix contained in the object.
+##
+## Author: Kevin Beland (belandbioinfo@gmail.com)
 
 ## Write a short comment describing this function
 
